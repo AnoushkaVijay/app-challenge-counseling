135bcb53992f1ceef1b26dc5aad63c95ea92977c
AnoushkaVijay <146681966+AnoushkaVijay@users.noreply.github.com>
2023-10-02 00:50:45 +0000

    added system prompt

--- pages/0_Survey.py
+++ pages/0_Survey.py
@@ -3,24 +3,20 @@
 import openai
 import streamlit as st
 
-with st.sidebar:
-    st.title('🤖💬 OpenAI Chatbot')
-    if 'OPENAI_API_KEY' in st.secrets:
-        st.success('API key already provided!', icon='✅')
-        openai.api_key = st.secrets['OPENAI_API_KEY']
-    else:
-        openai.api_key = st.text_input('Enter OpenAI API token:', type='password')
-        if not (openai.api_key.startswith('sk-') and len(openai.api_key)==51):
-            st.warning('Please enter your credentials!', icon='⚠️')
-        else:
-            st.success('Proceed to entering your prompt message!', icon='👉')
+openai.api_key = st.secrets['OPENAI_API_KEY']
 
+SYSTEM_PROMPT = '''
+You are a guidance counselor. Students come to you to get advice on college admissions. 
+Before providing advice, you ask questions to provide the best advice and guidance to these students. 
+'''
 if "messages" not in st.session_state:
     st.session_state.messages = []
+    st.session_state.messages.append({"role": "system", "content": SYSTEM_PROMPT})
 
 for message in st.session_state.messages:
-    with st.chat_message(message["role"]):
-        st.markdown(message["content"])
+    if message["role"] != "system" : 
+        with st.chat_message(message["role"]):
+            st.markdown(message["content"])
 
 if prompt := st.chat_input("What is up?"):
     st.session_state.messages.append({"role": "user", "content": prompt})
